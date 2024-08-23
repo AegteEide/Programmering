9cb017fff456b3144e8dfce7bf119c8dfb42f996
AegteEide <129384583+AegteEide@users.noreply.github.com>
2024-08-23 11:06:28 +0200

    Fixed template

--- 02_web_template_CSS_tricks/index.js
+++ 02_web_template_CSS_tricks/index.js
@@ -1,12 +1,27 @@
-let currentPage = 3
-let colors = ['red','green','blue','limegreen','lightcyan','lightgrey','lightblue']
+let currentPage = 1
+let menuNumber = 1
 
 let pages //array med alle elementer med class = page
+let menuItems //et array med alle menupunkterne
+let colors = ['red','green','blue','limegreen','lightcyan','lightgrey','lightblue']
 
 function setup(){
     select('#page' + currentPage).addClass('visible')
+    select('#menu' + menuNumber).addClass('active')
+
     console.log("P5.js er loaded")
     pages = selectAll('.page')
+    menuItems = selectAll('.menuitem')
+
+    for(m of menuItems){
+        m.mousePressed(function(e)  {
+            //e.target er selv HTML div'en id gør at vi kune vil have id'et
+            console.log(e.target.id)
+            //slice -1 henter det sidste bogstav i en string
+            let nr = e.target.id.slice(-1)
+            shiftPage(nr)
+        })
+    }
 
     //Lav en masse div'er vi kommer ind i page3
     for(c of colors){
@@ -30,6 +45,12 @@ function shiftPage(num){
     select("#page" + currentPage).removeClass('visible')
     currentPage = num
     select("#page" + currentPage).addClass('visible')
+
+    select("#menu" + menuNumber).removeClass('active')
+    menuNumber = num
+    select("#menu" + menuNumber).addClass('active')
+
+
 
 }
 
