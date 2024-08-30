02d92568ff6789f2650a05b55b0ed95f20278fac
AegteEide <129384583+AegteEide@users.noreply.github.com>
2024-08-30 15:08:06 +0200

    Udviklet videre på hjemmeside

--- 03_web_app_opgave/index.js
+++ 03_web_app_opgave/index.js
@@ -1,30 +1,34 @@
 let currentPage = 2
 
-let pages //array med alle elementer med class = page
-let menuItems //et array med alle menupunkterne
+let pages //array med alle elementer med class = page 
+let menuItems //array med alle menupunkterne  
 
 function setup(){
-    
+    //shiftPage er funktionen der tager et tal og skifter til en side
+
+
     pages = selectAll('.page')
     menuItems = selectAll('.menuitem')
-    
+
     //menu items skal reagere ved at skifte side
-    for(m of menuItems){
-        m.mousePressed(function(e)  {
-            //e.target er selv HTML div'en id gør at vi kune vil have id'et
+    for( m of menuItems ){
+        m.mousePressed( function(e) {
+            //e.target er selve html div'en 
             console.log(e.target.id)
             //slice -1 henter det sidste bogstav i en string
             let nr = e.target.id.slice(-1)
-            //Kalde shiftPage som skifter side til variablen nr
+            //nu kan vi kalde shiftPage som skifter side
             shiftPage(nr)
         })
     }
-    
-    
-    shiftPage(currentPage) //shiftpage er funktionen der tager et tal og skifter til en side
+        
+    shiftPage(currentPage)
+    //vent to sekunder og sæt så klassen "hidden" på headeren - så menuen bliver væk
+    setTimeout(function(){
+        select('header').addClass('hidden')
+    }, 10000)
 }
 
-//shiftPage funktionen
 function shiftPage(num){
     if(num == "ArrowLeft"){
         num = currentPage - 1
@@ -32,23 +36,20 @@ function shiftPage(num){
     if(num == "ArrowRight"){
         num = currentPage + 1
     }
+
     if(isNaN(num) || num > pages.length || num == 0){
         return
     }
+
     select("#page" + currentPage).removeClass('visible')
     select("#menu" + currentPage).removeClass('active')
     currentPage = num
-    
     select("#page" + currentPage).addClass('visible')
     select("#menu" + currentPage).addClass('active')
-
-
-
 }
-
-
 
 function keyPressed(){
     console.log(key)
     shiftPage(key)
 }
+
